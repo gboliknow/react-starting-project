38647ecd78e6e59c1bd9cdbf8dd239c11645558b
gbolahan <adekoyegbolahan001@gmail.com>
2024-06-24 15:35:03 +0100

    Splittig Components By Faeture ans state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 
 import { Fragment, useState } from 'react';
-import { CORE_CONCEPTS } from './data.js';
 import { Header } from './components/Header/Header.jsx';
-import { CoreConcepts } from './components/Coreconcepts.jsx';
-import { TabButton } from './components/TabButton.jsx'
-import { EXAMPLES } from './data.js';
-
-
-
 
 
 
@@ -20,38 +13,8 @@ function App() {
     <Fragment>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((_, index) => (
-              <CoreConcepts
-                {...CORE_CONCEPTS[index]}
-              />
-            ))}
-          </ul>
-        </section>
-        <section id='examples'>
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected={selectedTopic == 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton isSelected={selectedTopic == 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic == 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton isSelected={selectedTopic == 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
-          </menu>
-          {selectedTopic ? <div id="tab-content">
-
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-
-          </div> : <p> Please Select a topic</p>}
-
-        </section>
-
+      <CoreConcepts />
+        <Examples />
       </main>
     </Fragment>
   );
